623f4c460f9e633bf5c3e3424cc68ebff6b30d36
lima-wendel <wchristofer@gmail.com>
2021-07-12 12:30:47 -0300

    configurações iniciais

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,57 @@
 import styled from 'styled-components'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
+//const Title = styled.h1`
+//  font-size: 50px;
+//  color: ${({ theme }) => theme.colors.primary};
+//`
+
+const Box = styled.div`    
+  background: #ffffff;
+  broder-radius: 8px;
+`;
+//Box maiúsculo pq é um componente e não uma tag
+
+const MainGrid = styled.main`
+  width: 100%;
+  grid-gap: 10px;
+  padding: 16px;
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 500px;
+  .profileArea {
+    display: none;
+    @media(min-width: 860px) {
+      display: block;
+    }
+  }
+
+  @media(min-width: 860px) {
+    max-width: 1110px;
+    display: grid;
+    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
+    grid-template-columns: 160px 1fr 312px;
+}
+`;
+
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+  <MainGrid>
+    <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+      <Box>
+       <img src="https://github.com/lima-wendel.png"></img>
+      </Box>
+    </div>
+    <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+      <Box >
+        Comunidades
+      </Box>
+    </div>
+    <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+      <Box>
+        Bem vindo
+      </Box>
+    </div>
+  </MainGrid>
+  )
 }
